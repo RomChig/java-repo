a14130556eafabc40a381ca018b861369d314d63
Roman Ihnatov <45129375+RomChig@users.noreply.github.com>
2020-06-08 00:24:43 +0300

    Update AKULA.java

--- romario/src/com/company/AKULA.java
+++ romario/src/com/company/AKULA.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class AKULA {
     void akkula() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Халепа, у нас будет try_sex_again ? ");
+        System.out.println("Халепа, у нас будет sex_again ?");
         scanner.nextLine();
-        System.out.println("Конечно Нет, скажешь ты, НО it is not truth");
+        System.out.println("Конечно Нет, скажешь ты, НО it is not a truth");
     }
 }
